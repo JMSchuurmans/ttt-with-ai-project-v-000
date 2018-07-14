356837467ecd51a85ff7d718eebc3ef6cb9bdce2
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-14 17:44:31 +0000

    automatically backed up by learn

--- lib/tictactoeCLI.rb
+++ lib/tictactoeCLI.rb
@@ -1,62 +1,62 @@
-# class TicTacToeController
-#
-#   def call
-#     input = ""
-#
-#     while input != "exit"
-#       puts "Welcome to Tic Tac Toe!"
-#       puts "How many players today? 0, 1, or 2?"
-#
-#       input = gets.strip
-#
-#       case input
-#       when "0"
-#         zero_player_game
-#       when "1"
-#         one_player_game
-#       when "2"
-#         two_player_game
-#       end
-#     end
-#   end
-#
-#   def zero_player_game
-#     Game.player_1 = Players::Computer.new("X")
-#     Game.player_2 = Players::Computer.new("O")
-#   end
-#
-#   def one_player_game
-#     puts "Which token would you like to be? X or O?"
-#     input = gets.strip.upcase
-#     if input == "X"
-#       Game.player_1 = Players::Human.new("X")
-#       Game.player_2 = Players::Computer.new("O")
-#     elsif input == "O"
-#       Game.player_1 = Players::Human.new("O")
-#       Game.player_2 = Players::Computer.new("X")
-#     else
-#       puts "Invalid input."
-#     end
-#   end
-#
-#   def two_player_game
-#     puts "Player 1 will go first. Please choose X or O."
-#     input = gets.strip.upcase
-#     if input == "X"
-#       Game.player_1 = Players::Human.new("X")
-#       Game.player_2 = Players::Human.new("O")
-#     elsif input == "O"
-#       Game.player_1 = Players::Human.new("O")
-#       Game.player_2 = Players::Human.new("X")
-#     else
-#       puts "Invalid input."
-#     end
-#   end
-#
-#   game = Game.new
-#   game.play
-#
-#
-#
-#
-# end
+class TicTacToeController
+
+  def call
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to Tic Tac Toe!"
+      puts "How many players today? 0, 1, or 2?"
+
+      input = gets.strip
+
+      case input
+      when "0"
+        zero_player_game
+      when "1"
+        one_player_game
+      when "2"
+        two_player_game
+      end
+    end
+  end
+
+  def zero_player_game
+    Game.player_1 = Players::Computer.new("X")
+    Game.player_2 = Players::Computer.new("O")
+  end
+
+  def one_player_game
+    puts "Which token would you like to be? X or O?"
+    input = gets.strip.upcase
+    if input == "X"
+      Game.player_1 = Players::Human.new("X")
+      Game.player_2 = Players::Computer.new("O")
+    elsif input == "O"
+      Game.player_1 = Players::Human.new("O")
+      Game.player_2 = Players::Computer.new("X")
+    else
+      puts "Invalid input."
+    end
+  end
+
+  def two_player_game
+    puts "Player 1 will go first. Please choose X or O."
+    input = gets.strip.upcase
+    if input == "X"
+      Game.player_1 = Players::Human.new("X")
+      Game.player_2 = Players::Human.new("O")
+    elsif input == "O"
+      Game.player_1 = Players::Human.new("O")
+      Game.player_2 = Players::Human.new("X")
+    else
+      puts "Invalid input."
+    end
+  end
+
+  game = Game.new
+  game.play
+
+
+
+
+end
