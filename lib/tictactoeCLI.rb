09685e57457800b3ec8cd113ff1b9990dda8f436
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-14 17:57:12 +0000

    automatically backed up by learn

--- lib/tictactoeCLI.rb
+++ lib/tictactoeCLI.rb
@@ -31,13 +31,13 @@ class TicTacToeController
     if input == "X"
       Game.new.player_1 = Players::Human.new("X")
       Game.new.player_2 = Players::Computer.new("O")
+      Game.new.play
     elsif input == "O"
       Game.new.player_1 = Players::Human.new("O")
       Game.new.player_2 = Players::Computer.new("X")
     else
       puts "Invalid input."
     end
-    Game.new.play
   end
 
   def two_player_game
